9b59ff8f69f46c7b5a072299567af6ac987d5f2e
just-developer-ux <vavmax@ukr.net>
2024-04-03 21:34:20 +0300

    ...

---
+++ prices.sql
@@ -0,0 +1,2 @@
+INSERT INTO webproject.prices (from_supplier, for_client, created_at, deleted_at, good_id) VALUES (20, 25, '2024-04-01', null, 1);
+INSERT INTO webproject.prices (from_supplier, for_client, created_at, deleted_at, good_id) VALUES (30, 35, '2024-04-02', null, 2);

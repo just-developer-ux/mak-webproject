63569a0ba9f27ffe52dfb8858335c2c8d9b56b42
just-developer-ux <vavmax@ukr.net>
2024-04-23 09:06:30 +0300

    ...

--- src/main/java/ua/edu/nung/pz/controller/GoodsServlet.java
+++ src/main/java/ua/edu/nung/pz/controller/GoodsServlet.java
@@ -21,36 +21,19 @@ public class GoodsServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         GoodRepository goodRepository = new GoodRepository();
-        ArrayList<Good> goods = goodRepository.getByBrand("Naturalis");
+        ArrayList<Good>  goods = goodRepository.getByBrand("Naturalis");
 
         String body = goods.stream().map(good -> {
-            String[] carouselItems = new String[good.getPhoto().length];
-            for (int i = 0; i < good.getPhoto().length; i++)
-            {
-                String active = (i == 0) ? " active" : "";
-                carouselItems[i] = "<div class=\"carousel-item" + active + "\">\n" +
-                        "<img src=\"/img/" + (good.getPhoto().length > 0 ? good.getPhoto()[i] : "") + "\" class=\"d-block w-100\" alt=\"good image\">\n" +
-                        "</div>\n";
-            }
             return "<div class=\"col-12 col-sm-6 col-lg-4 col-xl-3 my-2\">" +
-                    "<div class=\"card\" style=\"width: 18rem;\">\n" +
-                    "<div id=\"carouselExample" + good.getId() + "\" class=\"carousel carousel-dark slide\">\n" +
-                    "  <div class=\"carousel-inner\">\n" +
-                    String.join("", carouselItems) +
-                    "  </div>\n" +
-                    "  <button class=\"carousel-control-prev\" type=\"button\" data-bs-target=\"#carouselExample" + good.getId() + "\" data-bs-slide=\"prev\">\n" +
-                    "    <span class=\"carousel-control-prev-icon\" aria-hidden=\"true\"></span>\n" +
-                    "    <span class=\"visually-hidden\">Previous</span>\n" +
-                    "  </button>\n" +
-                    "  <button class=\"carousel-control-next\" type=\"button\" data-bs-target=\"#carouselExample" + good.getId() + "\" data-bs-slide=\"next\">\n" +
-                    "    <span class=\"carousel-control-next-icon\" aria-hidden=\"true\"\"></span>\n" +
-                    "    <span class=\"visually-hidden\">Next</span>\n" +
-                    "  </button>\n" +
-                    "</div>" +
+                    "<div class=\"card\">\n" +
+                    "<img src=\"/img/" + (good.getPhoto().length > 0 ? good.getPhoto()[0] : "") + "\" class=\"card-img-top\" alt=\"good image\">" +
                     "  <div class=\"card-body\">\n" +
-                    "    <h5 class=\"card-title\">" + good.getName() + "</h5>\n" +
+                    "    <h5 class=\"card-title position-relative me-3\">" + good.getName() +
+                    "<span class=\"position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success\">" +
+                    good.getLikes() +
+                    "</span>" +
+                    "</h5>\n" +
                     "    <h6 class=\"card-subtitle mb-2 text-body-secondary\">Price:" + good.getPrice().getFor_client() + " UAH</h6>\n" +
-                    "    <h6 class=\"card-subtitle mb-2 text-body-secondary\">Likes:" + good.getLikes() + "</h6>\n" +
                     "    <p class=\"card-text\">" + good.getShortDescription() + "</p>\n" +
                     "    <a href=\"#\" class=\"card-link\">Card link</a>\n" +
                     "    <a href=\"#\" class=\"card-link\">Another link</a>\n" +
